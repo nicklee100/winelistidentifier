307919c2b878685dcbe42b722e48a477aebf2c83
Nick Lee <nicklee100@gmail.com>
2017-04-27 14:52:45 -0500

    sucessfully setup to get image text from google vision

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const express = requrie('express')
-
+const express = require('express')
+const Vision = require('@google-cloud/vision');
 const app = express()
+
+const projectId = 'winelist-165917';
+
+const visionClient = Vision({
+  projectId: projectId
+});
+
+const fileName = './list.jpg';
+
+visionClient.detectText(fileName)
+  .then((results) => {
+    const detections = results[0];
+    console.log('Text:');
+    detections.forEach((text) => console.log(text));
+
+  });
+
+
+
 
 app.get('/', function (req,res){
   res.send('server running')
