f14e1e0567c4f92211cf6e41dd21b300986fc6c5
Nick Lee <nicklee100@gmail.com>
2017-04-28 15:00:58 -0500

    wrote functions to parse through  winedotcom and google cloud data

--- index.js
+++ index.js
@@ -1,30 +1,111 @@
 const express = require('express')
-const Vision = require('@google-cloud/vision');
+const Vision = require('@google-cloud/vision')
+const request = require('request')
 const app = express()
+const requesting = require('request-promise')
+const prettyjson = require('prettyjson')
+const jsonFormat = require('json-format')
 
-const projectId = 'winelist-165917';
+const  wineApiKey= require('./apikey')
 
-const visionClient = Vision({
-  projectId: projectId
-});
 
+
+//wine.com variables
+const apipath = 'http://services.wine.com/api/beta2/service.svc/JSON/'
+const resoure = 'catalog'
+const parameters = 'search'
+let wineterms = ['2014', 'tolosa', 'no', 'oak','chardonnay']
+
+//google vision code
+const VisionProjectId = 'winelist-165917'
+const textinarray = []
 const fileName = './list.jpg';
 
-visionClient.detectText(fileName)
-  .then((results) => {
-    const detections = results[0];
-    console.log('Text:');
-    detections.forEach((text) => console.log(text));
 
-  });
+const visionClient = Vision({
+  projectId: VisionProjectId
+});
 
 
 
-
-app.get('/', function (req,res){
-  res.send('server running')
-})
-
-app.listen(3000, function() {
-  console.log('serving running on 3000')
+//google cloud
+function Reading(fileName) {
+  visionClient.detectText(fileName)
+    .then((results) => {
+     let detections = results[0][0];
+     return detections.split('\n')
+     console.log('Text:', detections);
+     //detections.forEach((text) => console.log(text));
+   })
+  .then(data => {
+   return data.filter(filterWineNames)
   })
+}
+
+//wine.com api
+
+function bottleLookUp(urlpath){
+ requesting(urlpath)
+   .then(function(data){
+    data = JSON.parse(data)
+    let WineResults = data.Products.List
+    //console.log(prettyjson.render(WineResults[0]))
+    let parsedResults = WineResults.map(wine => OrganizeData(wine))
+    console.log(parsedResults)
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+
+}
+
+//utility functions
+
+function filterWineNames(element){
+  return element.length > 5;
+}
+
+function OrganizeData(WineResults){
+      return  {
+      id: WineResults.Id,
+      name: WineResults.Name,
+      Region: WineResults.Appellation.Region.Name,
+      Vineyard: WineResults.Vineyard.Name,
+      Price: {
+        max: WineResults.PriceMax,
+        min: WineResults.PriceMin,
+        Retail: WineResults.PriceRetail
+      },
+      tastingNotes: WineResults.ProductAttributes[0].Name,
+      Reviews: {
+        community: WineResults.Community.Reviews.HighestScore,
+        general: WineResults.Ratings.HighestScore,
+      }
+    }
+
+}
+
+function searchBuilder(termsarray) {
+  let terms = termsarray.join('+');
+  let path = apipath + resoure + '?apikey=' + wineApiKey + '&' + parameters + '=' + terms
+  return path
+}
+
+
+//let urlpath = searchBuilder(array)
+
+bottleLookUp(searchBuilder(wineterms))
+
+
+
+
+
+
+
+// app.get('/', function (req,res){
+//   res.send('server running')
+// })
+
+// app.listen(3000, function() {
+//   console.log('serving running on 3000')
+//   })
